0aad40f5ad04d33a4d46cf59f3c09021bd9df9be
berbatvictor <120995152+berbatvictor@users.noreply.github.com>
2022-12-19 16:14:24 -0300

    Update Aplicacao_web.py

--- Aplicacao_web.py
+++ Aplicacao_web.py
@@ -14,7 +14,7 @@ st.write(""" Modelo previsor de Drawdown """)
          
         
 def p_to_f(x):
-         if % in A:
+         if '%' in A:
                   return float(x.strip('%'))/100
          else:
                   return float(x)/100
